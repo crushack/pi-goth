bb575f4241cc45e035a005674cb573f70067bbed
crushack <psilviu94@gmail.com>
2020-07-08 00:27:20 +0100

    Add glog & gflags modules and add small test.

--- src/goth_main.cpp
+++ src/goth_main.cpp
@@ -1,11 +1,17 @@
 #include <base/command/command.hpp>
+
+#include <glog/logging.h>
+
 #include <iostream>
 #include <memory>
 
-int main() 
+
+int main(int argc, char* argv[]) 
 {
+  google::InitGoogleLogging(argv[0]);
   std::shared_ptr<goth::base::command::Command> command =
       std::make_shared<goth::base::command::JumpCommand>();
   command->execute();
+  LOG(INFO) << "Logging working!";
   return 0;
 }
