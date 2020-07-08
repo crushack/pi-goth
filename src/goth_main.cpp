88739898f8150b55512e40ef7e46e750619c2c6d
crushack <psilviu94@gmail.com>
2020-07-08 19:32:36 +0100

    Initialize gflags in the main project.

--- src/goth_main.cpp
+++ src/goth_main.cpp
@@ -1,13 +1,14 @@
 #include <base/command/command.hpp>
 
+#include <gflags/gflags.h>
 #include <glog/logging.h>
 
 #include <iostream>
 #include <memory>
 
-
 int main(int argc, char* argv[]) 
 {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
   google::InitGoogleLogging(argv[0]);
   std::shared_ptr<goth::base::command::Command> command =
       std::make_shared<goth::base::command::JumpCommand>();
